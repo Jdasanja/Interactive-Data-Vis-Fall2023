d5335ff97cc0d187c99a12420c451fa40a76a10a
Jonathan Asanjarani <jdasanja@gmail.com>
2023-08-30 03:15:18 -0400

    Tutorial 3

--- 1_3_intro_to_js/main.js
+++ 1_3_intro_to_js/main.js
@@ -1,1 +1,10 @@
 console.log('hello world');
+let clicks = 0;
+function onClick() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks;
+  };
+//document.getElementById("clicks")--->looks for html id "clicks"
+//.innerHTML---> allows html element with id "clicks" to be modified
+//.innerHTML = clicks; ---> assigns a value to the inner html property with id "click" value. 
+//The value being assigned is represented by the variable clicks.
