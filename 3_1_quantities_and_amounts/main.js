a71d84867199850321de8ee601b69c727d16d2f7
Jonathan Asanjarani <jdasanja@gmail.com>
2023-11-23 16:24:20 -0500

    Update

--- 3_1_quantities_and_amounts/main.js
+++ 3_1_quantities_and_amounts/main.js
@@ -1,15 +1,20 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+height = window.innerHeight * 0.7,
+margin = {top: 20, bottom: 60, left: 60, right: 40 },
+radius = 5;
 
 // // since we use our scales in multiple functions, they need global scope
-// let xScale, yScale;
+let xScale; 
+let yScale;
+let svg;
+let color;
+color = d3.scaleOrdinal(d3.schemeAccent);
 
 /* APPLICATION STATE */
 let state = {
-  // data: [],
+   data: [],
+   selectedNationality: "All"
 };
 
 /* LOAD DATA */
@@ -24,8 +29,23 @@ d3.csv('../data/MoMA_topTenNationalities.csv', d3.autoType).then(raw_data => {
 // this will be run *one time* when the data finishes loading in
 function init() {
   /* SCALES */
+  xScale = d3.scaleLinear() 
+    .domain([0, 1500])
+    .range([0, width])
 
+  yScale = d3.scaleBand() 
+    .domain(["Nationality","American","German","British","French", "Italian","Japanese","Swiss","Dutch", "Russian", "Austrian"])
+    .range([height, 0])
+  
 
+  svg =d3.select("#container")
+    .append("svg")
+    .attr("height", height)
+    .attr("width", width)
+    .style("background-color", "aliceblue")
+
+  const xAxis = d3.axisBottom(xScale)
+  const yAxis = d3.axisLeft(yScale)
 
   draw(); // calls the draw function
 }
@@ -33,8 +53,30 @@ function init() {
 /* DRAW FUNCTION */
 // we call this every time there is an update to the data/state
 function draw() {
+  console.log(svg)
+  console.log(color)
   /* HTML ELEMENTS */
- 
+  //const color = d3.scaleOrdinal(d3.schemeAccent);
+  //bars
+  const bars = svg.selectAll("rect.bar")
+  .data(state.data)
+  .join("rect")
+  .attr("class", "bar")
+  .attr("x", 0)
+  .attr("y", d => yScale(d.Nationality))
+  .attr("height", yScale.bandwidth()) // Set the width based on scale bandwidth
+  .attr("width", d => xScale(d.Count))
+  .style("fill", d => color(d.Nationality)); 
+
+   // x-axis scale
+   svg.selectAll("text")
+    .data(state.data)
+    .enter()
+    .append("text")
+    .attr("x", 0)
+    .attr("y", d => yScale(d.Nationality) + 25)
+    .text(d => d.Nationality)
+    .style("text-anchor", "start");
 
 
 }
--- 3_2_distributions/main.js
+++ 3_2_distributions/main.js
@@ -1,15 +1,15 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+ const width = window.innerWidth * 0.7,
+   height = window.innerHeight * 0.7,
+   margin = {top: 20, bottom: 60, left: 60, right: 40 },
+   radius = 5;
 
 // these variables allow us to access anything we manipulate in init() but need access to in draw().
 // All these variables are empty before we assign something to them.
-// let svg;
-// let xScale;
-// let yScale;
-// let colorScale;
+ let svg;
+ let xScale;
+ let yScale;
+ let colorScale;
 
 /* APPLICATION STATE */
 let state = {
@@ -29,44 +29,118 @@ d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in
 function init() {
+  console.log(state)
   // + SCALES
+  xScale = d3.scaleLinear()
+    .domain([0, 1])
+    //.domain(d3.extent(state.data, d=> d.ideologyScore2020))
+    .range([margin.left, width - margin.right])
 
-
+  yScale = d3.scaleLinear()
+    .domain([0, 100])
+    .range([height - margin.bottom, margin.top])
+  
+  colorScale = d3.scaleOrdinal()
+    .domain(["R","D"])
+    .range(["red", "blue"])
+  
+   
   // + AXES
-
+  const xAxis = d3.axisBottom(xScale)
+  const yAxis = d3.axisLeft(yScale)
 
   // + UI ELEMENT SETUP
-
+  const selectElement = d3.select("#dropdown") 
+  
+// An alternative way to join the element selection to the data selection
+ //selectElement
+   //.selectAll("option")
+   //.data([...Array.from(new Set(state.data.map(d => d.Party))), "All"])
+   //.join("option")
+   //.attr("value", d =>d)
+   //.text(d => d)
+  
+  selectElement
+    .on("change", event => {
+      //console.log(event.target.value)
+      state.selectedParty = event.target.value;
+      draw();
+    })
 
   // + CREATE SVG ELEMENT
+  svg = d3.select("#container")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
 
-
-  // + CALL AXES
-
+  // + Call Axes
+  svg
+    .append("g")
+    .call(xAxis)
+    .attr("transform", `translate(0,${height - margin.bottom})`)
+  
+  svg
+    .append("g")
+    .call(yAxis)
+    .attr("transform", `translate(${margin.left},0)`)
 
 
   draw(); // calls the draw function
-}
+  }
 
-/* DRAW FUNCTION */
+/*----------------- DRAW FUNCTION----------------------------- */
 // we call this every time there is an update to the data/state
+
+
 function draw() {
 
   // + FILTER DATA BASED ON STATE
   const filteredData = state.data
-    // .filter(d => state.selectedParty === "All" || state.selectedParty === d.Party)
+    .filter(d => state.selectedParty === "All" || state.selectedParty === d.Party);
 
-  const dot = svg
-    .selectAll("circle")
-    .data(filteredData, d => d.BioID)
-    .join(
+    console.log(filteredData);
+
+
+svg.selectAll('circle.congress')
+  .data(filteredData, d => d.BioID)
+  .join(
+    enter => enter.append("circle")
+      .attr("r", 1)
+      .attr("cx", d => xScale(d.ideologyScore2020))
+      .attr("cy", d => yScale(d.envScore2020))
+      .call(sel => sel
+      .transition()
+      .duration(500)
+      .attr("r", 5)),
+    update => update,
+    exit => exit
+    .call(sel => sel
+      .transition()
+      .attr("r",0)
+    .remove()),
+  )
+  .attr("class", "congress")
+  .style("fill", d => colorScale(d.Party)) //ColorFill
+}
+ 
+
+
+
+
+
+  //
+    // 
+
+  //const dot = svg
+    //.selectAll("circle")
+    //.data(filteredData, d => d.BioID)
+    //.join(
       // + HANDLE ENTER SELECTION
-      enter => enter,
+      //enter => enter,
 
       // + HANDLE UPDATE SELECTION
-      update => update,
+      //update => update,
 
       // + HANDLE EXIT SELECTION
-      exit => exit
-    );
-}
+      //exit => exit
+    //);
